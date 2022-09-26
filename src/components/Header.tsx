fea9eafb41a89a1f5b37aa6c1b410ab0f3d48817
Augusto Dias <me@gutodias.dev>
2022-09-26 20:48:26 -0300

    refactor: change head title to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,26 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import { RiWhatsappLine } from 'react-icons/ri'
 
 export function Header () {
   return (
-    <section className='w-full flex justify-center'>
-      <div className='md:flex md:w-[90%] 2xl:w-[1400px] py-8 justify-between'>
-        <Link href='/'>
-          <h1 className='text-5xl font-thin text-slate-800 cursor-pointer'>
-            gutodias<span className='text-purple-700 font-bold'>.</span>dev
-          </h1>
-        </Link>
-        <a className='bg-[#075E54] rounded-full py-2 px-8 text-white font-bold xl:flex items-center gap-4 hidden' href='https://api.whatsapp.com/send?phone=5591984243773&text=Ol%C3%A1%2C%20tudo%20bem%3F' rel='noreferrer' target='_blank'>
-          Fale comigo
-          <RiWhatsappLine />
-        </a>
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Guto Dias - Desenvolvimento Web Fullstack</title>
+      </Head>
+      <section className='w-full flex justify-center'>
+        <div className='md:flex md:w-[90%] 2xl:w-[1400px] py-8 justify-between'>
+          <Link href='/'>
+            <h1 className='text-5xl font-thin text-slate-800 cursor-pointer'>
+              gutodias<span className='text-purple-700 font-bold'>.</span>dev
+            </h1>
+          </Link>
+          <a className='bg-[#075E54] rounded-full py-2 px-8 text-white font-bold xl:flex items-center gap-4 hidden' href='https://api.whatsapp.com/send?phone=5591984243773&text=Ol%C3%A1%2C%20tudo%20bem%3F' rel='noreferrer' target='_blank'>
+            Fale comigo
+            <RiWhatsappLine />
+          </a>
+        </div>
+      </section>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import { Header } from '../components/Header'
 import { HeroSection } from '../components/HeroSection'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Guto Dias - Desenvolvimento Web Fullstack</title>
-      </Head>
       <div>
         <Header />
         <HeroSection />
