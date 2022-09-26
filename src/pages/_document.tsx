6373fb90539f93e2d7ecbbb1838edeb7e00e3ee7
Augusto Dias <me@gutodias.dev>
2022-09-26 20:57:05 -0300

    refactor: change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document () {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <body className='font-source-sans'>
         <Main />
