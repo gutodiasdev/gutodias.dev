eadf8a8254dfea9e5b5ecac1b4fa37da31007167
Augusto <gutodiasdev@gmail.com>
2024-03-27 14:12:08 -0300

    refact: add cnpj and business name

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {year} Guto Dias. Todos os direitos reservados.
+        &copy; {year} JAYSON AUGUSTO DIAS DA SILVA LTDA | CNPJ: 52.287.782/0001-95. Todos os direitos reservados. 
       </small>
       <p className="text-xs">
         <span className="font-semibold">Sobre este site:</span> Foi desenvolvido com 
